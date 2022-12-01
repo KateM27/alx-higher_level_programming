695917cd6dc4e09e5c2a14e23aa0a1162eab4b11
KateM27 <mamocatherine@gmail.com>
2022-12-01 12:22:46 +0300

    updates

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,19 +6,19 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-  
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-  
+
   rotate () {
     const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
-  
+
   double () {
     this.width *= 2;
     this.height *= 2;
